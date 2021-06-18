2caf4d3e00cbe3d4e75102510d5aafea6dd4e9fa
davidcosama <davidcosama@github.com>
2021-06-18 22:16:12 +0200

    Adding logic to add the passport and express-session and setup SESSION_SECRET value on the .env file

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const passport = require('passport');
+const session = require('express-session');
 
 const app = express();
 app.set('view engine', 'pug');
@@ -10,6 +12,15 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// console.log('DAVID:DEBUG:session_secret', process.env.SESSION_SECRET);
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.route('/').get((req, res) => {
   res.render(process.cwd() + '/views/pug/index', {title: 'Hello', message: 'Please login'});
